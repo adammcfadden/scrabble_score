6a7ffb5bc607a08ff6688f8d954b564e396bb07a
Adam & David <student@epicodus.com>
2015-04-21 13:37:56 -0700

    final commit that allows the program to calculate the value of a given word.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -6,4 +6,8 @@ describe('String#scrabble_score') do
     expect(("z").scrabble_score()).to(eq(10))
   end
 
+  it("returns a scrabble score for a word") do
+    expect(("ship").scrabble_score()).to(eq(9))
+  end
+
 end
