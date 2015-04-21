a7f8dbabf72527b0983c0a9a5d2fd8d42a97c91b
Adam & David <student@epicodus.com>
2015-04-21 12:09:18 -0700

    Intial spec meet

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,7 +3,7 @@ require('scrabble_score')
 
 describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
-    expect("a".scrabble_score()).to(eq(1))
+    expect(("z").scrabble_score()).to(eq(10))
   end
-  
+
 end
